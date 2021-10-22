9b6db3acd83998cde62390b9e1f85cafab8c3a52
Yujin <46616793+bellayujin@users.noreply.github.com>
2021-10-22 10:48:15 +0900

    211022 수정사항

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
 function stopPainting(){
     painting = false;
+}
+
+function startPainting(){
+    painting = true;
 }
 
 function onMouseMove(event){
@@ -16,12 +24,9 @@ function onMouseDown(event){
     painting = true;
 }
 
-function onMouseUp(event){
-    stopPainting();
-}
-
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseUp", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseUp", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+}
